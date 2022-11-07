b3c9d91e0f181e1e3ccc6e4621b307c0df97049c
sungwoon129 <43958570+sungwoon129@users.noreply.github.com>
2022-11-07 15:17:55 +0900

    init

---
+++ Spring-Validation-Common-Module/src/test/java/com/blog/springvalidationcommonmodule/SpringValidationCommonModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.springvalidationcommonmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValidationCommonModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
