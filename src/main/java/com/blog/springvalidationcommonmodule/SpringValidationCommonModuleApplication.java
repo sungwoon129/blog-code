5c61a765e2de9c43dd28edbd14dc3077d35313bf
YOONSUNGWOON <sungwoon129@gmail.com>
2022-11-07 15:09:29 +0900

    init commit

---
+++ src/main/java/com/blog/springvalidationcommonmodule/SpringValidationCommonModuleApplication.java
@@ -0,0 +1,13 @@
+package com.blog.springvalidationcommonmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValidationCommonModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringValidationCommonModuleApplication.class, args);
+    }
+
+}
