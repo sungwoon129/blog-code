ea676644304f993fc4483b20c4df09cab2e50bcb
master <sungwoony@kakao.com>
2024-01-06 23:38:17 +0900

    Item36. 비트 필드 대신 EnumSet을 사용하라 : 클라이언트 코드 수정

--- effective-java/src/main/java/com/blog/items/item36/Client.java
+++ effective-java/src/main/java/com/blog/items/item36/Client.java
@@ -2,11 +2,14 @@ package com.blog.items.item36;
 
 import java.util.EnumSet;
 
+import static com.blog.items.item36.BadText.STYLE_BOLD;
+import static com.blog.items.item36.BadText.STYLE_ITALIC;
+
 public class Client {
     public void test() {
 
         BadText badTest = new BadText();
-        badTest.applyStyles(3);
+        badTest.applyStyles(STYLE_BOLD + STYLE_ITALIC);
 
         GoodText text = new GoodText();
         text.applyStyles(EnumSet.of(GoodText.Style.BOLD, GoodText.Style.UNDERLINE));
