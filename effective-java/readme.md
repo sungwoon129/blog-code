426a7ba4db1047ce296604506db59363cb965316
master <sungwoony@kakao.com>
2024-01-15 23:16:12 +0900

    Item 65. 리플렉션보다는 인터페이스를 사용하라

--- effective-java/readme.md
+++ effective-java/readme.md
@@ -7,3 +7,4 @@
 + [아이템 38](https://github.com/sungwoon129/blog-code/tree/main/effective-java/src/main/java/com/blog/items/item38)
 + [아이템 45](https://github.com/sungwoon129/blog-code/tree/main/effective-java/src/main/java/com/blog/items/item45)
 + [아이템 60](https://github.com/sungwoon129/blog-code/tree/main/effective-java/src/main/java/com/blog/items/item60)
++ [아이템 65](https://github.com/sungwoon129/blog-code/tree/main/effective-java/src/main/java/com/blog/items/item65)
---
+++ effective-java/src/main/java/com/blog/items/item65/Reflection.java
@@ -0,0 +1,49 @@
+package com.blog.items.item65;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Set;
+
+public class Reflection {
+    public void printSetByReflection(String[] classNames) {
+        Class<? extends Set<String>> cl = null;
+        try {
+            cl = (Class<? extends Set<String>>) Class.forName(classNames[0]);
+        } catch (ClassNotFoundException e) {
+            fatalError("클래스를 찾을 수 없습니다.");
+        }
+
+        Constructor<? extends  Set<String>> cons = null;
+        try {
+            assert cl != null;
+            cons = cl.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            fatalError("매개변수 없는 생성자를 찾을 수 없습니다.");
+        }
+
+        Set<String> s = null;
+        try {
+            assert cons != null;
+            s = cons.newInstance();
+        } catch (InvocationTargetException e) {
+            fatalError("생성자가 예외를 던졌습니다.");
+        } catch (InstantiationException e) {
+            fatalError("클래스를 인스턴스화 할 수 없습니다.");
+        } catch (IllegalAccessException e) {
+            fatalError("생성자에 접근할 수 없습니다.");
+        } catch (ClassCastException e) {
+            fatalError("Set을 구현하지 않은 클래스입니다.");
+        }
+
+        assert s != null;
+        s.addAll(Arrays.asList(classNames).subList(1, classNames.length));
+        System.out.println(s);
+
+    }
+
+    private static void fatalError(String msg) {
+        System.err.println(msg);
+        System.exit(1);
+    }
+}
