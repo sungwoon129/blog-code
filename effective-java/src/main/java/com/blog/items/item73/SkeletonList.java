1c71b8353642be58311f1fba908e553e45a95ff5
master <sungwoony@kakao.com>
2024-01-16 23:12:06 +0900

    Item 73. 추상화 수준에 맞는 예외를 던지라

---
+++ effective-java/src/main/java/com/blog/items/item73/SkeletonList.java
@@ -0,0 +1,56 @@
+package com.blog.items.item73;
+
+import java.util.*;
+
+public class SkeletonList {
+
+    public List<Integer> intArrayList(int[] a) {
+        Objects.requireNonNull(a);
+
+        return new AbstractList<>() {
+            @Override
+            public Integer get(int i) {
+                return a[i];
+            }
+
+            @Override
+            public Integer set(int i, Integer val) {
+                int oldVal = a[i];
+                a[i] = val;
+                return oldVal;
+            }
+
+            @Override
+            public int size() {
+                return a.length;
+            }
+        };
+    }
+
+    public List<Integer> intSeqArrayList(int[] a) {
+        Objects.requireNonNull(a);
+        return new AbstractSequentialList<>() {
+            @Override
+            public ListIterator<Integer> listIterator(int index) {
+                return listIterator(index);
+            }
+
+            public Integer get(int index) {
+                ListIterator<Integer> i = listIterator(index);
+
+                try {
+                    return i.next();
+                } catch (Exception e) {
+                    throw new IndexOutOfBoundsException("인덱스 : " + index);
+                }
+            }
+
+            @Override
+            public int size() {
+                return a.length;
+            }
+        };
+
+
+    }
+}
